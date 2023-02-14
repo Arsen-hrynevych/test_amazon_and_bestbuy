307bd7da51588572029216772fc48c29cda81c3a
Joan <joan@Joans-MacBook-Pro.local>
2023-02-14 18:21:17 +0000

    the environment is configured

---
+++ tests/test_shopping.py
@@ -0,0 +1,5 @@
+def test_shopping(driver):
+    product = 'samsung galaxy s22'
+    pass
+    # once script completed the line below should be uncommented.
+    # assert amazon_price > bestbuy_price
